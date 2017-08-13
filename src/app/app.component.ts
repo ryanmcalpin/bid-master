a46a15e57fb48af772fa8fc814b195b4a2e24587
Ryan McAlpin <rhmcalpin@gmail.com>
2017-08-13 16:53:25 -0700

    refactor to form builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'KlabbyPro';
+export class AppComponent implements OnInit {
+  title: String = 'KlabbyPro';
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+
+  }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      siding: ['', Validators.required],
+      soffit: ['', Validators.required],
+      fascia: ['', Validators.required],
+      glazing: ['', Validators.required],
+      foundation: ['', Validators.required],
+      scraping: ['', Validators.required],
+      windows: ['', Validators.required],
+      windowFrames: ['', Validators.required],
+      doors: ['', Validators.required],
+      doorFrames: ['', Validators.required],
+      obstructions: ['', Validators.required],
+      pillars: ['', Validators.required],
+      pressure: ['', Validators.required],
+      vegetation: ['', Validators.required],
+      additionalHrs: ['', Validators.required],
+      additionalFlat: ['', Validators.required]
+    })
   }
 
   calculateBid() {
-    alert("yaaa")
+    console.log(this.form.value.soffit)
   }
 }
