e9a6c67321f59f9c162e242943b3e80114802e57
Ryan McAlpin <rhmcalpin@gmail.com>
2018-03-30 22:19:46 -0700

    connect form to backend

--- src/app/bid-sheet-form/bid-sheet-form.component.ts
+++ src/app/bid-sheet-form/bid-sheet-form.component.ts
@@ -11,6 +11,23 @@ export class BidSheetFormComponent implements OnInit {
   title: String = 'KlabbyPro';
   form: FormGroup;
 
+  siding: number;
+  soffit: number;
+  fascia: number;
+  glazing: number;
+  foundation: number;
+  scraping: number;
+  windows: number;
+  windowFrames: number;
+  doors: number;
+  doorFrames: number;
+  obstructions: number;
+  pillars: number;
+  pressure: number;
+  vegetation: number;
+  additionalHrs: number;
+  additionalFlat: number;
+
   constructor(private fb: FormBuilder) {
 
   }
@@ -37,7 +54,24 @@ export class BidSheetFormComponent implements OnInit {
   }
 
   calculateBid() {
-    console.log(this.form.value.soffit)
+    console.log(this.form.value.soffit);
+
+    this.siding = this.form.value.siding;
+    this.soffit = this.form.value.soffit;
+    this.fascia = this.form.value.fascia;
+    this.glazing = this.form.value.glazing;
+    this.foundation = this.form.value.foundation;
+    this.scraping = this.form.value.scraping;
+    this.windows = this.form.value.windows;
+    this.windowFrames = this.form.value.windowFrames;
+    this.doors = this.form.value.doors;
+    this.doorFrames = this.form.value.doorFrames;
+    this.obstructions = this.form.value.obstructions;
+    this.pillars = this.form.value.pillars;
+    this.pressure = this.form.value.pressure;
+    this.vegetation = this.form.value.vegetation;
+    this.additionalHrs = this.form.value.additionalHrs;
+    this.additionalFlat = this.form.value.additionalFlat;
   }
 
 
