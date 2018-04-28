cc985e93be0f9d80e318f6b2f2900261d9586186
Rygn <rhmcalpin@gmail.com>
2018-04-27 18:14:30 -0700

    add rates component and nav to rates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
 import { BidSheetComponent } from './bid-sheet/bid-sheet.component';
 import { BidSheetFormComponent } from './bid-sheet-form/bid-sheet-form.component';
 import { HomeComponent } from './home/home.component';
+import { RatesComponent } from './rates/rates.component';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -26,7 +27,8 @@ export const firebaseConfig = {
     AppComponent,
     BidSheetComponent,
     BidSheetFormComponent,
-    HomeComponent
+    HomeComponent,
+    RatesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { HomeComponent } from './home/home.component'
 import { BidSheetFormComponent } from './bid-sheet-form/bid-sheet-form.component';
 import { BidSheetComponent } from './bid-sheet/bid-sheet.component';
+import { RatesComponent } from './rates/rates.component';
 
 const appRoutes: Routes = [
   {
@@ -17,6 +18,10 @@ const appRoutes: Routes = [
   {
     path: 'bids/:bidId',
     component: BidSheetComponent
+  },
+  {
+    path: 'rates',
+    component: RatesComponent
   }
 ];
 
