1bea7d9b4bbcb723ec797e636666d88da51ef84b
Rygn <rhmcalpin@gmail.com>
2018-05-08 18:57:56 -0700

    dynamically build rates form

--- src/app/rates/rates.component.ts
+++ src/app/rates/rates.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import 'rxjs/add/operator/takeUntil';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 import { Subject } from 'rxjs/Subject';
 import { DbService } from '../db.service';
 import { AuthService } from '../auth.service';
@@ -14,20 +15,36 @@ export class RatesComponent implements OnInit {
   user: any = null;
   private ngUnsubscribe: Subject<void> = new Subject<void>();
   rates: FirebaseListObservable<any>;
+  form: FormGroup;
 
-  constructor(public db: DbService, private auth: AuthService) { }
+  constructor(public db: DbService,
+              private auth: AuthService,
+              private fb: FormBuilder) { }
 
   ngOnInit() {
     this.auth.getCurrentUser().takeUntil(this.ngUnsubscribe).subscribe(user => {
-        if (user) {
-          this.db.getUserObjectById(user.uid).takeUntil(this.ngUnsubscribe).subscribe(userObj => {
-            this.user = userObj;
+      if (user) {
+        this.db.getUserObjectById(user.uid).takeUntil(this.ngUnsubscribe).subscribe(userObj => {
+          this.user = userObj;
+        })
+        this.db.getRates(user.uid).takeUntil(this.ngUnsubscribe).subscribe(rates => {
+          this.rates = rates;
+          this.form = this.fb.group({
+            doorFramesPerHour: [rates[0].$value, Validators.required],
           })
-          this.db.getRates(user.uid).takeUntil(this.ngUnsubscribe).subscribe(rates => {
-              this.rates = rates;
-            })
-        }
-      })
+          for (let i = 1; i < rates.length; i++) {
+            let newControl = new FormControl(['', Validators.required]);
+            newControl.setValue(rates[i].$value);
+            this.form.addControl(rates[i].$key, newControl);
+          }
+        })
+      }
+    })
+
+  }
+
+  saveRates() {
+    console.log('forrm')
   }
 
 }
